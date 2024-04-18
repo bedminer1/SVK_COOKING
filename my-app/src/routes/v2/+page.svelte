6b8336c0a2318c34b4378972ec83a839147497f5
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-18 21:05:11 +0800

    flying out transition done

--- my-app/src/routes/v2/+page.svelte
+++ my-app/src/routes/v2/+page.svelte
@@ -3,12 +3,14 @@
     import image1 from "$lib/images/1.jpeg"
 	import image2 from "$lib/images/2.jpeg"
 	import image3 from "$lib/images/3.jpeg"
+    import { fly } from 'svelte/transition'
 
 	const images: string[] = [image1, image2, image3]
     let liked = new Set<string>()
     let disliked = new Set<string>()
     let direction: string;
     let index: number = 0
+    let transitionActive: boolean
     
     function handler(event: CustomEvent) { 
         direction = event.detail.direction;
@@ -18,6 +20,12 @@
             || direction === "top"
             || direction === "bottom")
                  return
+
+        // TRANSITION
+        transitionActive = true
+        setTimeout(() => {
+            transitionActive = false
+        }, 500)
 
         // RIGHT FOR LIKE, LEFT FOR DISLIKE
         if (direction === "right") {
@@ -32,14 +40,16 @@
     }
     </script>
     
-    <div class="w-full h-screen flex items-center justify-center">
-        <div use:swipe={{ timeframe: 300, minSwipeDistance: 60 }} on:swipe={handler}>
+    <div class="w-full h-screen flex items-center justify-center"
+    >
+        <div use:swipe={{ timeframe: 600, minSwipeDistance: 10 }} on:swipe={handler}>
             direction: {direction}
             {#if index == images.length}
                 <p>OUT OF SWIPES</p>
-            {:else}
+            {:else if !transitionActive}
                 <img src={images[index]} 
                 alt="" 
+                out:fly={{duration: 100, x: direction === "right" ? 200 : -200}}
                 class="w-full h-[40vh] pointer-events-none select-none"
                 >
             {/if}
